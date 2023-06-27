c86cb488c571368177b5f0c7d10e400ed08a66d0
AkulaPhanidhar <akulaphanidhar30@gmail.com>
2023-06-27 22:55:09 +0530

    hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,15 +5,20 @@ const Hero = () => {
   return (
     <>
       <div className="hero">
-        <div className="heroContainer">
-          <p className="line1">
-            Complete your Profile to see eligible universities
-          </p>
-          <p className="line2">
-            Provide your details like school, college, CGPA, test scores, etc to
-            get best universities and recommendations for your Profile
-          </p>
-          <button className="button">Complete Profile</button>
+        <div className="upperContainer">
+          <div className="heroContainer">
+            <p className="line1">
+              <p className="subLine">Complete</p>
+              <p className="subLine2">your profile</p>
+              <p className="subLine">to see</p>
+              <p className="subLine3">Eligible Universities</p>
+            </p>
+            <p className="line2">
+              Provide your details like school, college, CGPA, test scores, etc
+              to get best universities and recommendations for your Profile
+            </p>
+            <button className="button">Complete Profile</button>
+          </div>
         </div>
       </div>
     </>
